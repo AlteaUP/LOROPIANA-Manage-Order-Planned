8465fcc6233bb9ba36235f3a6ce4b2b7ff18093f
SalvatoreLa <salvatore.la.spata@gmail.com>
2025-04-10 14:10:16 +0200

    adjustment

--- app/manageplannedorder/webapp/ext/main/Main.controller.js
+++ app/manageplannedorder/webapp/ext/main/Main.controller.js
@@ -1,8 +1,11 @@
 sap.ui.define(
     [
-        'sap/fe/core/PageController'
+        'sap/fe/core/PageController',
+        'sap/ui/model/json/JSONModel',
+        'sap/ui/model/Filter',
+        'sap/ui/model/FilterOperator'
     ],
-    function (PageController) {
+    function (PageController, JSONModel, Filter, FilterOperator) {
         'use strict';
 
         return PageController.extend('manageplannedorder.manageplannedorder.ext.main.Main', {
@@ -41,6 +44,79 @@ sap.ui.define(
             //
             //  }
             pressFragment: function (e) {
+                debugger;
+                const oTable = e.getSource().getParent().getParent().getParent()
+                const selectedItems = oTable.getSelectedItems()
+                const oModel = this.getOwnerComponent().getModel()
+                const _selectedItems = []
+                for (let i = 0; i < selectedItems.length; i++) {
+                    const oObj = selectedItems[i].getBindingContext().getObject()
+                    _selectedItems.push(oObj)
+                }
+                const model = new JSONModel()
+                model.setData({ TypeOrder: 'antani', selectedItems: _selectedItems })
+
+                if (!this._fragmentPezze) {
+                    this._fragmentPezze = this.loadFragment({
+                        id: "fragmentPezze",
+                        name: "manageplannedorder.manageplannedorder.ext.fragment.Converti",
+                        controller: this
+                    });
+                }
+
+                this._fragmentPezze.then(function (dialog) {
+                    // dialog.bindElement(`/ZZ1_MFP_ASSIGNMENT/(SAP_UUID='${obj.SAP_UUID}')`);
+                    dialog.setModel(model, 'selected');
+                    dialog.setModel(oModel)
+                    const tabella = dialog.getContent().at(-1);
+                    // { 
+                    //     path: '/ZZ1_MFP_ASSIGNMENT',
+                    //     parameters : { $$updateGroupId : 'CreatePezzeBatch' }
+                    // }
+                    // compose filter from selected items
+                    const filters = []
+                    _selectedItems.forEach((item) => {
+                        const filter = new Filter("FSH_CPLND_ORD", FilterOperator.EQ, item.CplndOrd)
+                        filters.push(filter)
+                    })
+
+                    tabella.bindAggregation('items', {
+                        path: '/ConvertPLO',
+                        filters,
+                        template: new sap.m.ColumnListItem({
+                            cells: [
+                                new sap.m.ObjectIdentifier({
+                                    title: "{FSH_CPLND_ORD}"
+                                }),
+                                new sap.m.Text({
+                                    text: "{AUART}"
+                                }),
+                                new sap.m.Text({
+                                    text: "{TOT_QTY}"
+                                }),
+                                new sap.m.Text({
+                                    text: "{UNIT}"
+                                })
+                            ]
+                        }),
+                        templateShareable: true,
+                        parameters: { $$updateGroupId: 'CreateConvertPLO' },
+                    });
+
+                    const binding = tabella.getBinding('items');
+                    binding.resetChanges()
+
+                    _selectedItems.forEach((item) => {
+
+                        binding.create({
+                            FSH_CPLND_ORD: item.CplndOrd,
+                            AUART: item.AUART,
+                            TOT_QTY: parseFloat(item.TotQty),
+                            UNIT: item.Unit,
+                        });
+                    });
+                    dialog.open();
+                }.bind(this));
                 // const oModel = this.getOwnerComponent().getModel();
                 // const oList = oModel.bindList("/ConvertPLO");
 
@@ -65,6 +141,14 @@ sap.ui.define(
                 //     MessageToast.show("Do Assemble error.");
                 //     console.error("Error", oError);
                 // });
+            },
+            onCloseDialog: function (oEvent) {
+                const oDialog = oEvent.getSource().getParent();
+                oDialog.close();
+                oDialog.destroy();
+            },
+            doConvert: function (oEvent) {
+                debugger;
             }
         });
     }
