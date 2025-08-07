06b5ce8e70da0d7acd5ddd5309225727721d437c
Davide Lilla <dlilla@alteanet.it>
2025-08-07 13:13:59 +0000

    Rimuovo 'dati/' dal versionamento, ora ignorato da .gitignore

--- app/manageplannedorder/dist/ext/controller/ChangeWorkCenter.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/m/MessageToast"],function(e){"use strict";return{doChangeWorkCenter:function(n){e.show("Custom handler invoked.")}}});
-//# sourceMappingURL=ChangeWorkCenter.js.map
